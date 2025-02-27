10241e20d5243b52b5febf376bff32a2ef02e75b
Akhil Krishnan Namanattu Sunilkumar <akhilkrishnanns@live.com>
2025-02-27 12:04:44 +0000

    updated with portfolio web contents. intial commit

---
+++ src/data.js
@@ -0,0 +1,20 @@
+export const projects = [
+    {
+      title: "Cusotom Video Conferencing for High Court of Kerala, India",
+      subtitle: "Vue js, SpringBoot",
+      description:
+        "Spearheaded the backend development for custom video conference applications, completing the project two weeks ahead of schedule using the TDD approach using SpringBoot, Auth0, VConsol, Kafka and AWS",
+      image: "/khc-vconsol.png",
+      link: "https://hckerala.vconsol.com/",
+    },
+  
+  ];
+
+  export const skills = ["Java","JavaScript","Python",".NET" ,"Django", "Spring MVC", "SpringBoot", "HTML", "CSS", "SCSS", "TypeScript", "Vue js", "Node js", "Nuxt js", "React js", "AWS", "GCP", "EC2", "ECS", "Pinpoint", "S3", "Lambda", "Elastic Search", "Firebase", "Kafka", "Git", "MySQL", "MongoDB", "PostgreSQL" , "Linux", "Auth0", "Gravitee", "IAM", "Kubernetes", "Jenkins","JUnit", "Jest",  "Agile", "Jira", "Confluence","Kanban", "TDD", "BDD"];
+  export const testimonials = [
+    {quote:"While working with Akhil, he gave us his maximum potential to build code and confidence on our products and commitments.",
+      image:"/creaturetech-logo.png",
+      name:"Mr. Pradeep Nagaraj",
+      company:"Creaturetch Innolabs"
+    }
+  ]
